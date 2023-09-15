ba2f852abb873e1a97fe49682a27fb759abd0c3c
abdullah ejaz <abdullahejaz@abdullahs-MacBook-Pro.local>
2023-09-15 12:54:52 +0500

    intilized the repo

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+           int studeTAgeNOW = 
+            
+
+        }
+    }
+}
